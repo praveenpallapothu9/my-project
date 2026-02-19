754b084e025e0ed8ba978683731b9189c4dd2e9a
Praveen Pallapothu <praveenpallpothu9@gmail.com>
2026-02-19 15:56:29 +0530

    numbers expansion

--- python_programming/python_programming/number_expansion_calculator.py
+++ python_programming/python_programming/number_expansion_calculator.py
@@ -1,2 +1,8 @@
 numbers = int(input("enter the number:"))
-print(numbers)
+print(f"the given number is :",numbers)
+sum =0
+while numbers > 0:
+    sum =  sum+numbers
+    digits = numbers % 10
+    numbers = numbers // 10
+print(f"the output that we got is :",sum)
