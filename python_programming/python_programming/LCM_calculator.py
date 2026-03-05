2af0dc84204a85500e6aa22c7cc3a9070791e795
Praveen Pallapothu <praveenpallpothu9@gmail.com>
2026-03-05 10:48:27 +0530

    init

---
+++ python_programming/python_programming/LCM_calculator.py
@@ -0,0 +1,14 @@
+x=15
+y=17
+
+if x > y:
+    larger = x
+else:
+    larger = y
+#print(larger)
+
+for i in range(1,larger+1):
+    if (larger % i == 0) and (larger % i == 0):
+        lcm = i
+        print(lcm)
+
